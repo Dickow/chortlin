545365190220e2d83d548bda438ba10ab4dde6dd
Dickow <Jeppedic@gmail.com>
2018-08-01 15:48:07 +0200

    Fixed two probable bugs.

--- core/src/main/kotlin/com/dickow/chortlin/core/configuration/trigger/Trigger.kt
+++ core/src/main/kotlin/com/dickow/chortlin/core/configuration/trigger/Trigger.kt
@@ -8,9 +8,4 @@ data class Trigger(internal val endpoint: Any,
                    internal val mapper: IMapper,
                    internal val processor: Any,
                    internal val interactions: Collection<Interaction>,
-                   internal val channel: Any) : ChortlinConfiguration {
-
-    override fun hashCode(): Int {
-        return endpoint.hashCode()
-    }
-}
+                   internal val channel: Any) : ChortlinConfiguration
