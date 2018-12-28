0c1fd80677ae5057acfcc8c7ba360ebc323af00d
Dickow <Jeppedic@hotmail.com>
2018-12-28 11:21:43 +0100

    removed unused import

--- checker/src/main/kotlin/com/dickow/chortlin/checker/choreography/method/ObservableMethod.kt
+++ checker/src/main/kotlin/com/dickow/chortlin/checker/choreography/method/ObservableMethod.kt
@@ -1,6 +1,5 @@
 package com.dickow.chortlin.checker.choreography.method
 
 import com.dickow.chortlin.checker.choreography.participant.InternalParticipant
-import java.lang.reflect.Method
 
 data class ObservableMethod(val method: String, val participant: InternalParticipant)
